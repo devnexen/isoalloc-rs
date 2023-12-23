2a315eb3886ad8dadc6e703e1dca02286f9a36ad
David Carlier <devnexen@gmail.com>
2023-12-23 21:37:01 +0000

    preparing next year major release

--- src/lib.rs
+++ src/lib.rs
@@ -20,13 +20,27 @@ unsafe impl GlobalAlloc for IsoAlloc {
     }
 
     #[inline]
-    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
-        iso_free(ptr as *mut c_void);
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        iso_free_size(ptr as *mut c_void, layout.size());
     }
 
     #[inline]
     unsafe fn realloc(&self, ptr: *mut u8, _: Layout, size: usize) -> *mut u8 {
         iso_realloc(ptr as *mut c_void, size) as *mut u8
+    }
+}
+
+impl IsoAlloc {
+    pub fn alloc_size(&self, ptr: *mut u8) -> usize {
+        unsafe { iso_chunksz(ptr as *mut c_void) }
+    }
+
+    pub fn mem_usage(&self) -> u64 {
+        unsafe { iso_alloc_mem_usage() }
+    }
+
+    pub fn leaks(&self) -> u64 {
+        unsafe { iso_alloc_detect_leaks() }
     }
 }
 
@@ -44,6 +58,33 @@ mod tests {
             p = core::ptr::null_mut();
             p = IsoAlloc.realloc(p, l, 4096);
             IsoAlloc.dealloc(p, l);
+        }
+    }
+
+    #[test]
+    fn large_alloc() {
+        unsafe {
+            let l = Layout::from_size_align(1 << 20, 32).unwrap();
+            let p = IsoAlloc.alloc(l);
+            IsoAlloc.dealloc(p, l);
+        }
+    }
+
+    #[test]
+    fn utils() {
+        use core::convert::TryInto;
+        unsafe {
+            let l = Layout::from_size_align(8, 8).unwrap();
+            let a = IsoAlloc.alloc(l);
+            assert!(IsoAlloc.alloc_size(a) >= l.size());
+            let ta = IsoAlloc.mem_usage();
+            assert!(ta > l.size().try_into().unwrap());
+            let b = IsoAlloc.alloc(l);
+            let tb = IsoAlloc.mem_usage();
+            assert!(tb >= ta);
+            IsoAlloc.dealloc(b, l);
+            IsoAlloc.dealloc(a, l);
+            assert_eq!(IsoAlloc.leaks(), 0u64);
         }
     }
 }
